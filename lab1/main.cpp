d2bd9a419891a3ce2444e271e9106d2f1b6c298e
LidiaMil <lidamilos@rambler.ru>
2022-02-18 00:56:59 +0300

    lab 1 add task 3

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,10 +9,36 @@
 
 using namespace std;
 
-template<typename T> 
-void showForTask7(string typeName)
+
+union Bit16
 {
-	cout << typeName << ":\t" << sizeof(T) << endl;
+    void* checking;
+    short* sht;
+    unsigned short* usht;
+};
+// void showForTask3(string showName, int32_t num)
+template<class T>
+void showForTask3(string showName, T t)
+{
+    Bit16 s = {t};
+	cout << ">>" << showName << ":\n";
+// 	cout << "Unsigned hex: " << hex << (uint16_t)num << endl; //беззнаковую интерпретацию в шестнадцатеричном представлении
+// 	cout << "Signed hex: " << hex << (int16_t)num << endl;    //знаковую интерпретацию в шестнадцатеричном представлении
+// 	cout << "Binary x16:   " << bitset<16>(num) << endl;      //беззнаковую интерпретацию в двоичном представлении; 
+// 	//знаковую интерпретацию в двоичном представлении;  ???????????
+//  	cout << "Binary x16:   " << bitset<16>(num) << endl;      //знаковую интерпретацию в двоичном представлении;  
+//  	cout << "Unsigned dec: " << dec << (uint16_t)num << endl; //беззнаковую интерпретацию в десятичном представлении;
+// 	cout << "Signed dec:   " << dec << (int16_t)num << endl;  //знаковую интерпретацию в десятичном представлении. 
+    cout << "Unsigned hex: " << hex << *s.usht << endl; //беззнаковую интерпретацию в шестнадцатеричном представлении
+    cout.unsetf(ios::hex);
+	cout << "Signed hex: " << hex << *s.sht << endl;    //знаковую интерпретацию в шестнадцатеричном представлении
+	cout.unsetf(ios::hex);
+	cout << "Binary x16:   " << std::bitset<sizeof(*s.usht) * CHAR_BIT>(*s.usht) << endl;      //беззнаковую интерпретацию в двоичном представлении; 
+ 	cout << "Binary x16:   " <<  std::bitset<sizeof(*s.sht) * CHAR_BIT>(*s.sht) << endl;      //знаковую интерпретацию в двоичном представлении;  
+ 	cout << "Unsigned dec: " << dec <<*s.usht  << endl; //беззнаковую интерпретацию в десятичном представлении;
+	cout.unsetf(ios::dec);
+	cout << "Signed dec:   " <<  dec << *s.sht  << endl; 
+    cout.unsetf(ios::dec);
 }
 
 
@@ -26,36 +52,77 @@ unsigned int* на выбранной платформе.
 запятой. Какие из них имеют разрядность 16, 32, 64 бита, учитывая, что байт
 x86/x86-64 — октет (8 бит)?
 */
+template<typename T> 
+void showForTask2(string typeName)
+{
+	cout << typeName << ":\t" << sizeof(T) << endl;
+}
 
 void task2()
 {
 	cout << endl << "---TASK 2---" << endl;
-	showForTask7<char>("char");
-	showForTask7<signed char>("signed char");
-	showForTask7<unsigned char>("unsigned char");
-	showForTask7<wchar_t>("wchar_t");
-	showForTask7<short>("short");
-	showForTask7<int>("int");
-    showForTask7<unsigned int>("unsigned int");
-	showForTask7<long>("long");
-	showForTask7<unsigned long>("unsigned long");
-	showForTask7<long long>("long long");
-	showForTask7<unsigned long long>("unsigned long long");
-	showForTask7<float>("float");
-	showForTask7<double>("double");
-	showForTask7<long double>("long double");
-	showForTask7<size_t>("size_t");
-	showForTask7<ptrdiff_t>("ptrdiff_t");
-	showForTask7<void*>("void*");
-	showForTask7<char*>("char*");
-	showForTask7<int*>("int*");
-	showForTask7<unsigned int*>("unsigned int*");
-// 	showForTask7<bool>("bool");
+	showForTask2<char>("char");
+	showForTask2<signed char>("signed char");
+	showForTask2<unsigned char>("unsigned char");
+	showForTask2<wchar_t>("wchar_t");
+	showForTask2<short>("short");
+	showForTask2<int>("int");
+    showForTask2<unsigned int>("unsigned int");
+	showForTask2<long>("long");
+	showForTask2<unsigned long>("unsigned long");
+	showForTask2<long long>("long long");
+	showForTask2<unsigned long long>("unsigned long long");
+	showForTask2<float>("float");
+	showForTask2<double>("double");
+	showForTask2<long double>("long double");
+	showForTask2<size_t>("size_t");
+	showForTask2<ptrdiff_t>("ptrdiff_t");
+	showForTask2<void*>("void*");
+	showForTask2<char*>("char*");
+	showForTask2<int*>("int*");
+	showForTask2<unsigned int*>("unsigned int*");
 }
 
+/*
+Изучите, как интерпретируется одна и та же область памяти,
+если она рассматривается как знаковое или беззнаковое целое число, а также —
+как одно и то же число записывается в различных системах счисления.
+Для этого на языке C/C++ разработайте функцию void 𝑝𝑟𝑖𝑛𝑡16(void * 𝑝),
+которая печатает для 16-битной области памяти по заданному адресу 𝑝:
+а) целочисленную беззнаковую интерпретацию в шестнадцатеричном представлении; 
+б) целочисленную беззнаковую интерпретацию в двоичном представлении; 
+в) целочисленную беззнаковую интерпретацию в десятичном представлении;
+г) целочисленную знаковую интерпретацию в шестнадцатеричном представлении;
+д) целочисленную знаковую интерпретацию в двоичном представлении;
+е) целочисленную знаковую интерпретацию в десятичном представлении. 
+*/
+
+void print16(int16_t x,int16_t y,int16_t z)
+{
+	cout << endl << "---TASK 3---" << endl;
+	unsigned short minInt16UnS = 0;
+	showForTask3("минимальное целое 16-битное значение без знака", &minInt16UnS);
+	cout << endl;
+	unsigned short maxInt16UnS = (unsigned short)USHRT_MAX;
+	showForTask3("максимальное целое 16-битное значение без знака", &maxInt16UnS);
+	cout << endl;
+	short minInt16S = SHRT_MIN;
+	showForTask3("минимальное целое 16-битное значение со знаком", &minInt16S);
+	cout << endl;
+	short maxInt16S = SHRT_MAX;
+	showForTask3("максимальное целое 16-битное значение со знаком", &maxInt16S);
+	cout << endl;
+	showForTask3("X", &x);
+	cout << endl;
+	showForTask3("Y", &y);
+	cout << endl;
+	showForTask3("Z", &z);
+}
 
 int main()
 {	
 	task2();
+	print16(0x8A8B8C8D,6,-3);
 
 }
+
